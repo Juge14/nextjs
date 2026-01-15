90c0b5340100dd8a1d8c41f98e7b6156dc0a7951
Juge14 <151007386+Juge14@users.noreply.github.com>
2026-01-15 10:46:18 +0200

    feat: add NotFound and Error components, handle invoice fetching errors, and improve invoice deletion functionality

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -5,6 +5,7 @@ import { z } from "zod"
 import { revalidatePath } from "next/cache"
 import { redirect } from "next/navigation"
 import postgres from "postgres"
+import { error } from "console"
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' })
 
@@ -27,11 +28,17 @@ export async function createInvoice(formData: FormData) {
   const amountInCents = amount * 100
   const date = new Date().toISOString().split('T')[0]
 
-  await sql`
+  try {
+    await sql`
     INSERT INTO invoices (customer_id, amount, status, date)
     VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
   `
-
+  } catch (error) {
+    console.error(error)
+    return {
+      message: 'Database error: Failed to create invoice'
+    }
+  }
   revalidatePath('/dashboard/invoices')
   redirect('/dashboard/invoices')
 }
@@ -46,12 +53,27 @@ export async function updateInvoice(id: string, formData: FormData) {
   })
 
   const amountInCents = amount * 100
-
-  await sql`
+    
+  try {
+    await sql`
     UPDATE invoices
     SET customer_id = ${customerId}, amount = ${amount}, status = ${status}
     WHERE id = ${id}
   `
+  } catch (error) {
+    console.error(error)
+    return {
+      message: 'Database error: Failed to update invoice'
+    }
+  }
+  
   revalidatePath('/dashboard/invoices')
   redirect('/dashboard/invoices')
+}
+
+export async function deleteInvoice(id: string) {
+  // throw error('error deleting')
+
+  await sql`DELETE FROM invoices WHERE id = ${id}`
+  revalidatePath('/dashboard/invoices')
 }
